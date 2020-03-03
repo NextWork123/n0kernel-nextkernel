d3a7eb213d05ba13291c57f721c17abb086a5110
Dimitar Yurukov <mscalindt@protonmail.com>
2020-03-03 15:25:03 +0200

    mm: Remove process reclaim
    Another broken meme from CAF that uses VM pressure to, uh, "improve" things.
    
    Signed-off-by: Dimitar Yurukov <mscalindt@protonmail.com>
    (cherry picked from commit 747e184d0f24414f2ddf8f73c398aedeccaa2d73)
    (cherry picked from commit a0756266f69ea199c2d07e89c219b2d2709663db)
    (cherry picked from commit b9e9fb21fdbeb28a2e513671cb86af67d3097cef)
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3036,25 +3036,5 @@ static inline void setup_nr_node_ids(void) {}
 
 extern int want_old_faultaround_pte;
 
-#ifdef CONFIG_PROCESS_RECLAIM
-struct reclaim_param {
-	struct vm_area_struct *vma;
-	/* Number of pages scanned */
-	int nr_scanned;
-	/* max pages to reclaim */
-	int nr_to_reclaim;
-	/* pages reclaimed */
-	int nr_reclaimed;
-};
-extern struct reclaim_param reclaim_task_anon(struct task_struct *task,
-		int nr_to_reclaim);
-extern struct reclaim_param reclaim_task_nomap(struct task_struct *task,
-		int nr_to_reclaim);
-extern int reclaim_address_space(struct address_space *mapping,
-		struct reclaim_param *rp);
-extern int proc_reclaim_notifier_register(struct notifier_block *nb);
-extern int proc_reclaim_notifier_unregister(struct notifier_block *nb);
-#endif
-
 #endif /* __KERNEL__ */
 #endif /* _LINUX_MM_H */
