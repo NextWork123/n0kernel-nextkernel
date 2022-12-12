e5e9f08e35400038999b147ffdb9c2fc5afc544a
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2022-12-12 11:11:06 +0100

    sched/fair: Reduce sched migration cost to 2ms
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -132,7 +132,7 @@ unsigned int sysctl_sched_child_runs_first __read_mostly = 1;
 unsigned int sysctl_sched_wakeup_granularity		= 10000000UL;
 static unsigned int normalized_sysctl_sched_wakeup_granularity	= 10000000UL;
 
-unsigned int __read_mostly sysctl_sched_migration_cost	= 500000UL;
+unsigned int __read_mostly sysctl_sched_migration_cost	= 200000UL;
 DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
 
 #ifdef CONFIG_SMP
